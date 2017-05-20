be39fd3987f7e24a1497b16be2ad4c556968cbcd
James Duncan <james.a.duncan91@gmail.com>
2017-05-20 10:32:12 +0100

    added front and back shiny images

--- public/app.js
+++ public/app.js
@@ -26,12 +26,27 @@ var populatePokemon = function(pokemon){
   var div = document.getElementById('pokemon-container')
   var imgDiv = document.createElement('div')
   var heading = document.createElement('h1')
+  var normalSpriteHeading = document.createElement('h3')
+  var shinySpriteHeading = document.createElement('h3')
   var frontImg = document.createElement('img')
+  var backImg = document.createElement('img')
+  var shinyFrontImg = document.createElement('img')
+  var shinyBackImg = document.createElement('img')
   capitialisedName = pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
   heading.innerText = capitialisedName;
+  normalSpriteHeading.innerText = "Normal";
+  shinySpriteHeading.innerText = "Shiny";
   frontImg.src = pokemon.sprites.front_default;
+  backImg.src = pokemon.sprites.back_default;
+  shinyFrontImg.src = pokemon.sprites.front_shiny;
+  shinyBackImg.src = pokemon.sprites.back_shiny;
 
+  imgDiv.appendChild(normalSpriteHeading)
   imgDiv.appendChild(frontImg)
+  imgDiv.appendChild(backImg)
+  imgDiv.appendChild(shinySpriteHeading)
+  imgDiv.appendChild(shinyFrontImg)
+  imgDiv.appendChild(shinyBackImg)
   div.appendChild(heading)
   div.appendChild(imgDiv)
   };
